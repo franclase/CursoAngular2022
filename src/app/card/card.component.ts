a2a4265ee9ff159e7db9f0a33e4852fe98221bcd
fran <franclase2@gmail.com>
2022-07-07 09:55:34 +0200

    Version 1

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  public image:string="https://photoshop-kopona.com/uploads/posts/2019-05/1559108923_0-2.jpg";
+  public titulo:string="Curso de Angular con Interpolación";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
